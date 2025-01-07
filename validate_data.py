96fb2e44a98fef3012dc8975dc088ee7d1ad2ec9
Walid Mougharbel <walidsmougharbel@gmail.com>
2025-01-07 21:54:39 +0200

    added check for booleans and outliers

--- validate_data.py
+++ validate_data.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from scipy.stats import zscore
 
 #print data in file for easier visualization
 def	print_data_in_file(file):
@@ -22,6 +23,31 @@ def	check_data_in_file(file_name, data_file):
 	if data_file.duplicated().sum().sum() != 0:
 		print(f"Error: duplicate found in {file_name}")
 		return False
+	#check for invalid booleans
+	if not data_file['hasYard'].isin([0,1]).any():
+		print(f"Invalid boolean value {data_file['hasYard']} in {file_name} 'hasYard'")
+		return False
+	if not data_file['hasPool'].isin([0,1]).any():
+		print(f"Invalid boolean value in {file_name} 'hasPool'")
+		return False
+	if not data_file['isNewBuilt'].isin([0,1]).any():
+		print(f"Invalid boolean value in {file_name} 'isNewBuilt'")
+		return False
+	if not data_file['hasStormProtector'].isin([0,1]).any():
+		print(f"Invalid boolean value in {file_name} 'hasStormProtector'")
+		return False
+	if not data_file['hasStorageRoom'].isin([0,1]).any():
+		print(f"Invalid boolean value in {file_name} 'hasStorageRoom'")
+		return False
+	return True
+
+def look_for_outliers(file):
+	z_scores = file.apply(zscore)
+	threshold = 2
+	outliers = (z_scores.abs() > threshold).any(axis=1)
+	if outliers.any():
+		print(f"Outliers were found on {file[outliers]}")
+		return False
 	return True
 
 #load files and validate data
@@ -39,9 +65,15 @@ def load_and_validate_files(train_file, test_file):
 		print ("Error: could not read files.")
 		return False
 
+	train = train.iloc[:, 1:]
+	test = test.iloc[:, 1:]
 	#check data for duplicates or null values
 	if not check_data_in_file(train_file, train) or \
 		not check_data_in_file(test_file ,test):
+		return False
+
+	#check for outliers
+	if not look_for_outliers(train) or not look_for_outliers(test):
 		return False
 
 	#check columns for inconsistent data between two files
